551a528967badaa3e5c6d910fc014f0d77d12178
SirPotatoIV <otoolej33@gmail.com>
2019-12-30 13:45:40 -0600

    JS for server currently working with js for public. One route has been created and tested succesfully. Updates are a little of a mess right now. Still figuring out how to organize all the files so everything works correctly.

---
+++ api/Answers.js
@@ -0,0 +1,31 @@
+class Answers {
+    constructor(answer) {
+        this.answer = answer;
+    }
+    createAnswerHtml(){
+        let answerHtml = "";
+        const answer = this.answer;
+        // console.log("create blanks worked!")
+        for(let i=0; i < answer.length; i++){
+            const character = answer[i];
+            if(character === " " || `'`){
+                answerHtml = answerHtml + `<div class="nonLetter">${answer[i]}</div>`;
+            }else{
+                answerHtml = answerHtml + `<div class="${answer[i]}">${answer[i]}</div>`;
+            }
+        }
+        console.log(answerHtml);
+    }
+
+    displayAnswerHtml(){
+        const answerDisplayEl = document.getElementById("answerDisplay");
+        console.log(answerDisplayEl);
+    }
+
+}
+
+const firstAnswer = new Answers("The Matrix");
+// console.log(firstAnswer.answer)
+firstAnswer.displayAnswerHtml()
+
+module.exports = Answers
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,8 +13,10 @@ var db = require("../models");
 module.exports = function(app) {
 
   // GET route for getting all of the todos
-  app.get("/api/todos", function(req, res) {
+  app.get("/api/answers", function(req, res) {
     // Write code here to retrieve all of the todos from the database and res.json them
+    const result = "hello"
+    return res.json(result)
     // back to the user
   });
 
